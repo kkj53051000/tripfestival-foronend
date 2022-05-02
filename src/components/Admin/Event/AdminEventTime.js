bab77ee2511fb90b3a5f340c8b798850abd8b725
kkj53051000 <tkarnrwl7418@naver.com>
2022-05-02 23:13:44 +0900

    fix AdminEventTime (Upload)

--- src/components/Admin/Event/AdminEventTime.js
+++ src/components/Admin/Event/AdminEventTime.js
@@ -9,6 +9,22 @@ const AdminEventTime = () => {
                 <div className="admin-event-time-item">
                     <span className="title">PK + 축제 이름</span>
                     <br/><br/>
+
+                    <span>제목</span>
+                    <input name="title" />
+                    <br/><br/>
+
+                    <span>시작 시간</span>
+                    <input name="startTime" type="time" />
+                    <br/><br/>
+
+                    <span>끝나는 시간</span>
+                    <input name="endTime" type="time" />
+                    <br/><br/>
+
+                    <button>업로드</button>
+                    <br/><br/><br/><br/>
+
                     <input placeholder="이벤트 시간" />
                     <button>수정</button>
                     <button>삭제</button>
