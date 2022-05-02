19b73a7d09677c9fb2834d4b14241094501b35be
kkj53051000 <tkarnrwl7418@naver.com>
2022-05-02 23:02:12 +0900

    fix AdminEventHashTag (Upload)

--- src/components/Admin/Event/AdminEventHashTag.js
+++ src/components/Admin/Event/AdminEventHashTag.js
@@ -5,9 +5,15 @@ const AdminEventHashTag = () => {
     return (
         <div className="admin-event-hash-tag-wrap">
             <h2>이벤트 해시태그</h2>
+
+            <h2>리스트</h2>
             <div className="admin-event-hash-tag-list">
                 <div className="admin-event-hash-tag-item">
                     <span className="title">PK + 해시태그 이름</span>
+                    <br/><br/>
+                    <span>해시태그 이름</span>
+                    <input name="name" />
+                    <button>추가</button>
                     <br/><br/>
                     <span>#해시태그</span><button>삭제</button>
                 </div>
