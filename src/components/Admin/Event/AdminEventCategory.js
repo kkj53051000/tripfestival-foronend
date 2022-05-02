bb6e675835247fd43590169d289fd44323853b8a
kkj53051000 <tkarnrwl7418@naver.com>
2022-05-02 22:55:28 +0900

    fix AdminEventCategory (Upload)

--- src/components/Admin/Event/AdminEventCategory.js
+++ src/components/Admin/Event/AdminEventCategory.js
@@ -5,6 +5,22 @@ const AdminEventCateogry = () => {
     return (
         <div className="admin-event-catgory-wrap">
             <h2>이벤트 카테고리</h2>
+
+            <h2>업로드</h2>
+            <div className="admin-event-category-upload">
+                <span>이름</span>
+                <input name="name" />
+                <br/><br/>
+
+                <span>사진</span>
+                <input name="img" type="file" />
+                <br/><br/>
+                
+                <button>업로드</button>
+            </div>
+
+
+            <h2>리스트</h2>
             <div className="admin-event-category-list">
                 <div className="item">
                     <input placeholder="카테고리명" />
