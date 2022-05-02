e2fc3e752d5d49cc5b38fc4bfb15c6bc6a378b8e
kkj53051000 <tkarnrwl7418@naver.com>
2022-05-02 23:25:31 +0900

    fix AdminLandmarkTime (Upload)

--- src/components/Admin/Landmark/AdminLandmarkTime.js
+++ src/components/Admin/Landmark/AdminLandmarkTime.js
@@ -9,6 +9,22 @@ const AdminLandmarkTime = () => {
                 <div className="admin-landmark-time-item">
                     <span className="title">PK + 관광지 이름</span>
                     <br/><br/>
+
+                    <span>제목</span>
+                    <input name="title" />
+                    <br/><br/>
+
+                    <span>시작 시간</span>
+                    <input type="time" />
+                    <br/><br/>
+
+                    <span>끝나는 시간</span>
+                    <input type="time" />
+                    <br/><br/>
+
+                    <button>업로드</button>
+                    <br/><br/><br/><br/>
+
                     <input placeholder="관광지 시간" />
                     <button>수정</button>
                     <button>삭제</button>
