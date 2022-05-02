2f2914b7b9e58bff959e6c9850887020b7f145c0
kkj53051000 <tkarnrwl7418@naver.com>
2022-05-02 23:09:05 +0900

    fix AdminEventSeason (Upload)

--- src/components/Admin/Event/AdminEventSeason.js
+++ src/components/Admin/Event/AdminEventSeason.js
@@ -5,6 +5,22 @@ const AdminEventSeason = () => {
     return (
         <div className="admin-event-season-wrap">
             <h2>이벤트 시즌</h2>
+
+            <h2>업로드</h2>
+            <div className="admin-event-season-upload">
+                <span>이름 (*)</span>
+                <input name="name" />
+                <br/><br/>
+
+                <span>이미지 (1개)</span>
+                <input type="file" />
+                <br/><br/>
+
+                <button>업로드</button>
+                <br/><br/><br/><br/>
+            </div>
+
+            <h2>리스트</h2>
             <div className="admin-event-season-list">
                 <div className="item">
                     <input placeholder="1월" />
