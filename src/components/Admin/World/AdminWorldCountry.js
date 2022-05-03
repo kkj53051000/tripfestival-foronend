9956de944e37c5ec7286bda30a71b706794868ab
kkj53051000 <tkarnrwl7418@naver.com>
2022-05-03 10:57:18 +0900

    fix AdminWorldCountry (Axios Upload)

--- src/components/Admin/World/AdminWorldCountry.js
+++ src/components/Admin/World/AdminWorldCountry.js
@@ -1,7 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import '../../../css/AdminWorldCountry.css';
+import axios from "axios";
 
 const AdminWorldCountry = () => {
+
+
+    // Name
+    const [name, setName] = useState(null);
+    const handleName = e => {
+        setName(e.target.value);
+    }
+
+    // Currency
+    const [currency, setCurrency] = useState(null);
+    const handleCurrency = e => {
+        setCurrency(e.target.value);
+    }
+
+    // Capital
+    const [capital, setCapital] = useState(null);
+    const handleCapital = e => {
+        setCapital(e.target.value);
+    }
+
+    // ExchangeRatio
+    const [exchangeRatio, setExchangeRatio] = useState(null);
+    const handleExchangeRatio = e => {
+        setExchangeRatio(e.target.value);
+    }
+
+
+    // File
+    const [file, setFile] = useState(null);
+
+    const handleFile = e => {
+        setFile(e.target.files[0])
+    }
+
+    var formData = new FormData();
+    formData.append("file", file);
+
+    const value = {
+        name: name,
+        currency: currency,
+        capital: capital,
+        exchangeRatio: exchangeRatio
+    }
+
+    formData.append("value", new Blob([JSON.stringify(value)], {type: "application/json"}));
+
+    const onClickUpload = async () => {
+        try {
+            const response = await axios.post("/api/worldCountryProcess", formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            });
+
+            console.log(response)
+        } catch(e) {
+            console.log(e);
+        }
+    }
+
     return (
         <div className="admin-world-country-wrap">
             <h2>세계 나라</h2>
@@ -10,26 +71,26 @@ const AdminWorldCountry = () => {
 
             <div className="admin-world-country-upload">
                 <span>이름</span>
-                <input name="name" />
+                <input name="name" onChange={handleName} />
                 <br/><br/>
 
                 <span>국기</span>
-                <input type="file" />
+                <input name="file" type="file" onChange={handleFile} />
                 <br/><br/>
 
                 <span>통화</span>
-                <input name="currency" />
+                <input name="currency" onChange={handleCurrency} />
                 <br/><br/>
 
                 <span>수도</span>
-                <input name="capital" />
+                <input name="capital" onChange={handleCapital} />
                 <br/><br/>
 
                 <span>환율</span>
-                <input name="환율" />
+                <input name="exchangeRatio" onChange={handleExchangeRatio} />
                 <br/><br/>
 
-                <button>업로드</button>
+                <button onClick={onClickUpload}>업로드</button>
             </div>
 
             <h2>리스트</h2>
