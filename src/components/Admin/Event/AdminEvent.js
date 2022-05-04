ec3f0b05c25045fcc511de2e02883e604b479755
kkj53051000 <tkarnrwl7418@naver.com>
2022-05-04 12:41:40 +0900

    fix AdminEvent : add Upload, List

--- src/components/Admin/Event/AdminEvent.js
+++ src/components/Admin/Event/AdminEvent.js
@@ -1,70 +1,232 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../../css/event/AdminEvent.css";
+import axios from "axios";
+import useApiGet from "../../../lib/useApiGet";
 
 const AdminEvent = () => {
+
+    const url = "/admin/event/event"
+
+    // Name
+    const [name, setName] = useState(null);
+    const handleName = e => {
+        setName(e.target.value);
+    }
+
+    // Description
+    const [description, setDescription] = useState(null);
+    const handleDiscription = e => {
+        setDescription(e.target.value);
+    }
+
+    // Adress
+    const [address, setAddress] = useState(null);
+    const handleAddress = e => {
+        setAddress(e.target.value);
+    }
+
+    // Visitor
+    const [visitor, setVisitor] = useState(null);
+    
+    const handleVisitor = e => {
+        setVisitor(e.target.value);
+    }
+
+    // Inout
+    const [inout, setInout] = useState(null);
+    const handleInout = e => {
+        setInout(e.target.value)
+    }
+
+    // WorldCountryCityRegion
+    const [worldCountryCityRegionId, setWorldCountryCityRegionId] = useState(null);
+    const handleWorldCountryCityRegion = e => {
+        setWorldCountryCityRegionId(e.target.value);
+    }
+
+    // EventCategory
+    const [eventCategoryId, setEventCategoryId] = useState(null);
+    const handleEventCategory = e => {
+        setEventCategoryId(e.target.value);
+    }
+
+    // EventSeason
+    const [eventSeasonId, setEventSeasonId] = useState(null);
+    const handleEventSeason = e => {
+        setEventSeasonId(e.target.value);
+    }
+
+    const value = {
+        name: name,
+        description: description,
+        address: address,
+        visitor: visitor,
+        inout: inout,
+        worldCountryCityRegionId: worldCountryCityRegionId,
+        eventCategoryId : eventCategoryId,
+        eventSeasonId : eventSeasonId
+    }
+
+    // Axios Upload
+    const onClickUpload = async () => {
+        try {
+            const response = await axios.post("/api/admin/eventProcess", value);
+
+            if(response.data.status === 'SUCCESS') {
+                alert("업로드 성공");
+                window.location.replace(url)
+            }
+        } catch(e) {
+            console.log(e);
+        }
+    }
+
+
+    // Evnet List
+    const [eventListLoading, eventList, eventListError] = useApiGet(() => {
+        return axios.get("/api/eventAllList");
+    }, []);
+
+
+    // WorldCountryCityRegion List
+    const [worldCountryCityRegionListLoading, worldCountryCityRegionList, worldCountryCityRegionListError] = useApiGet(() => {
+        return axios.get("/api/worldCountryCityRegionList");
+    }, []);
+
+    // EventCategory List
+    const [eventCategoryListLoading, eventCategoryList, eventCategoryListError] = useApiGet(() => {
+        return axios.get("/api/eventCategoryAllList");
+    }, []);
+
+    // EventSeason List
+    const [eventSeasonListLoading, eventSeasonList, eventSeasonListError] = useApiGet(() => {
+        return axios.get("/api/eventSeasonAllList");
+    }, []);
+
+
     return (
         <div className="admin-event-upload-wrap">
             <h2>이벤트(축제) 업로드</h2>
 
             <div className="admin-event-upload">
                 <span>이름</span>
-                <input /><br/><br/>
+                <input onChange={handleName} /><br/><br/>
                 <span>설명</span>
-                <textarea /><br/><br/>
+                <textarea onChange={handleDiscription} /><br/><br/>
                 <span>주소</span>
-                <input /><br/><br/>
+                <input onChange={handleAddress}  /><br/><br/>
                 <span>방문객</span>
-                <input /><br/><br/>
+                <input onChange={handleVisitor} /><br/><br/>
                 <span>inout</span>
-                <input /><br/><br/>
+                <input placeholder="1 밖 0 안" onChange={handleInout} /><br/><br/>
 
                 <span>리젼</span>
-                <select>
-                    <option>어딘가</option>
+                <select onChange={handleWorldCountryCityRegion}>
+                    <option disabled selected>
+                        선택하시오
+                    </option>
+                    {worldCountryCityRegionList != null && !worldCountryCityRegionListLoading ?
+                    <>
+                        {worldCountryCityRegionList.data.items.map(region => (
+                            <option value={region.id}>
+                                {region.name}
+                            </option>
+                        ))}
+                    </>
+                    :
+                    <></>
+                    }
                 </select>
                 <br/><br/>
 
                 <span>카테고리</span>
-                <select>
-                    <option>무언가</option>
+                <select onChange={handleEventCategory}>
+                    <option disabled selected>
+                        선택하시오
+                    </option>
+                    {eventCategoryList != null && !eventCategoryListLoading ?
+                    <>
+                        {eventCategoryList.data.items.map(category => (
+                            <option value={category.id}>
+                                {category.name}
+                            </option>
+                        ))}
+                    </>
+                    :
+                    <></>
+                    }
                 </select>
                 <br/><br/>
 
                 <span>시즌</span>
-                <select>
+                <select onChange={handleEventSeason}>
                     <option>1월</option>
+                    {eventSeasonList != null && !eventSeasonListLoading ?
+                    <>
+                        {eventSeasonList.data.items.map(season => (
+                            <option value={season.id}>
+                                {season.name}
+                            </option>
+                        ))}
+                    </>
+                    :
+                    <></>
+                    }
                 </select>
                 <br/><br/>
 
 
 
-                <button>업로드</button>
+                <button onClick={onClickUpload}>업로드</button>
             </div>
 
             <h2>이벤트(축제) 리스트</h2>
 
             <div className="admin-event-list">
-                <span className="title">PK + 축제 이름</span>
+                {eventList != null && !eventListLoading ?
+                <>
+                    {eventList.data.items.map(event => (
+                        <>
+                        <span className="title">{event.id} {event.name}</span>
 
-                <br/>
+                        <br/>
 
-                <input className="name" placeholder="축제 이름" />
+                        이름 : <input className="name" placeholder={event.name} />
+                        <br/><br/>
 
-                <textarea className="description" placeholder="설명 내용" />
+                        설명 : <textarea className="description" placeholder={event.description} />
+                        <br/><br/>
 
-                <input className="adress" placeholder="주소 내용" />
+                        주소 : <input className="address" placeholder={event.address} />
+                        <br/><br/>
 
-                <input className="visitor" placeholder="방문객 수" />
+                        방문객 : <input className="visitor" placeholder={event.visitor} />
+                        <br/><br/>
+                        
+                        밖안 : <input className="inout" placeholder={event.inout ? "밖":"안"} />
+                        <br/><br/>
 
-                <input className="worldcountrycityregion" placeholder="리전Id" />
+                        worldCountryCityRegionId : <input className="worldcountrycityRegionId" placeholder={event.worldCountryCityRegionId} />
+                        <br/><br/>
 
-                <input className="eventCategory" placeholder="이벤트 카테고리Id" />
+                        eventCategoryId : <input className="eventCategoryId" placeholder={event.eventCategoryId} />
+                        <br/><br/>
 
-                <input className="eventSeason" placeholder="이벤트 시즌Id (월)" />
+                        eventSeasonId : <input className="eventSeasonId" placeholder={event.eventSeasonId} />
+                        <br/><br/>
 
-                <br/><br/>
+                        <br/><br/>
 
-                <button>수정</button>
+                        <button>삭제</button>
+                        <button>수정</button>
+                        <br/><br/>
+                        </>
+                    ))}
+                </>
+                :
+                <></>
+                }
+                
             </div>
         </div>
     );
--- src/components/Admin/Event/AdminEventTime.js
+++ src/components/Admin/Event/AdminEventTime.js
@@ -1,30 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
 import '../../../css/event/AdminEventTime.css';
+import axios from "axios";
 
 const AdminEventTime = () => {
+
+    const url = "/admin/event/time";
+
+    // Title
+    const [title, setTitle] = useState(null);
+
+    const handleTitle = e => {
+        setTitle(e.target.value);
+    }
+
+    // Start Time
+    const [startTime, setStartTime] = useState(null);
+
+    const handleStartTime = e => {
+        setStartTime(e.target.value);
+    }
+
+    // End Time
+    const [endTime, setEndTime] = useState(null);
+
+    const handleEndTime = e => {
+        setEndTime(e.target.value);
+    }
+
+    const value = {
+        title : title,
+        startTime : startTime,
+        endTime : endTime
+    }
+    // Axios Upload
+    const onClickUpload = async () => {
+        try {
+            const response = await axios.post("/api/admin/eventTimeProcess", value)
+
+            if(response.data.status === 'SUCCESS') {
+                alert("업로드 성공");
+                window.location.replace(url)
+            }
+        } catch(e) {
+            console.log(e);
+        }
+    }
+
+    // Event List
+    // const [eventListLoading, eventList, eventListError] = useApiGet(() => {
+    //     return axios.get("");
+    // }, []);
+
+
     return (
         <div className="admin-event-time-wrap">
-            <h2>이벤트 시간</h2>
+            <h2>이벤트 시간 업로드</h2>
+
+            <div className="admin-event-time-upload">
+                <span className="title">PK + 축제 이름</span>
+                <br/><br/>
+
+                <span>제목</span>
+                <input name="title" onChange={handleTitle} />
+                <br/><br/>
+
+                <span>시작 시간</span>
+                <input name="startTime" type="time" onChange={handleStartTime} />
+                <br/><br/>
+
+                <span>끝나는 시간</span>
+                <input name="endTime" type="time" onChange={handleEndTime} />
+                <br/><br/>
+
+                <span>축제</span>
+                <select>
+
+                </select>
+                <br/><br/>
+
+                <button onClick={onClickUpload}>업로드</button>
+                <br/><br/><br/><br/>
+            </div>
+
             <div className="admin-event-time-list">
                 <div className="admin-event-time-item">
-                    <span className="title">PK + 축제 이름</span>
-                    <br/><br/>
-
-                    <span>제목</span>
-                    <input name="title" />
-                    <br/><br/>
-
-                    <span>시작 시간</span>
-                    <input name="startTime" type="time" />
-                    <br/><br/>
-
-                    <span>끝나는 시간</span>
-                    <input name="endTime" type="time" />
-                    <br/><br/>
-
-                    <button>업로드</button>
-                    <br/><br/><br/><br/>
-
                     <input placeholder="이벤트 시간" />
                     <button>수정</button>
                     <button>삭제</button>
