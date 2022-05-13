891f0ddee34c473f3905ab95f2aff31330d8968a
kkj53051000 <tkarnrwl7418@naver.com>
2022-05-13 18:13:33 +0900

    fix FestivalCategoryList : add Select

--- src/components/Festival/FestivalCategoryList.js
+++ src/components/Festival/FestivalCategoryList.js
@@ -1,7 +1,71 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import "../../css/festival/FestivalCategoryList.css";
+import useApiGet from "../../lib/useApiGet";
 
 const FestivalCategoryList = () => {
+
+    // Event Category Id
+    const [categoryId, setCategoryId] = useState(null);
+    const handleCategoryId = e => {
+        setCategoryId(e.target.value);
+    }
+
+    // City
+    const [cityId, setCityId] = useState(0);
+    const handleCityId = e => {
+        setCityId(e.target.value);
+    }
+
+    // Region
+    const [regionId, setRegionId] = useState(0);
+    const handleRegionId = e => {
+        setRegionId(e.target.value);
+    }
+
+    const [landmarkList, setLandmarkList] = useState(null);
+
+
+
+    // Axios Upload
+    const onClickBtn = async () => {
+        try {
+
+            const response = await axios.get("/api/eventCategoryList", {
+                params: {
+                    eventCategoryId: categoryId,
+                    worldCountryCityId: cityId,
+                    worldCountryCityRegionId: regionId   
+                }
+            })
+
+            setLandmarkList(response.data.items)
+
+        } catch(e) {
+            console.log(e);
+        }
+    }
+
+
+    // Event Category
+    const [categoryListLoading, categoryList, categoryListError] = useApiGet(() => {
+        return axios.get("/api/eventCategoryAllList");
+    }, [])
+
+    // City List
+    const [cityListLoading, cityList, cityListError] = useApiGet(() => {
+        return axios.get("/api/worldCountryCityNameList");
+    }, [])
+
+    // Region List
+    const [regionListLoading, regionList, regionListError] = useApiGet(() => {
+        return axios.get("/api/worldCountryCityRegion", {
+            params: { 
+                worldCountryId: cityId,
+            }
+        })
+    }, [cityId])
+
     return (
         <div className="festival-category-wrap">
             <div className="festival-category-select">
@@ -11,18 +75,25 @@ const FestivalCategoryList = () => {
 
                 <div className="one">
                     <div class="style">
-                        <select>
-                            <option>전체</option>
-                            <option> 선택 2 </option>
-                            <option> 선택 3 </option>
-                            <option> 선택 4 </option>
-                            <option> 선택 5 </option>
+                        <select onChange={handleCategoryId}>
+                            <option selected disabled>선택하시오</option>
+                            {categoryList != null && !categoryListLoading ?
+                            <>
+                                {categoryList.data.items.map(category => (
+                                    <option value={category.id} key={category.id}>
+                                        {category.name}
+                                    </option>
+                                ))}
+                            </>
+                            :
+                            <></>
+                            }
                         </select>
                     </div>
                 </div>
 
                 <div className="btn">
-                    <button>검색</button>
+                    <button onClick={onClickBtn}>검색</button>
                 </div>
             </div>
 
@@ -32,23 +103,38 @@ const FestivalCategoryList = () => {
                 </div>
                 <div className="search-one">
                     <div class="style">
-                        <select>
-                            <option>전체</option>
-                            <option> 선택 2 </option>
-                            <option> 선택 3 </option>
-                            <option> 선택 4 </option>
-                            <option> 선택 5 </option>
+                        <select onChange={handleCityId}>
+                            <option selected disabled>선택하시오</option>
+                            {cityList != null && !cityListLoading ?
+                            <>
+                                {cityList.data.items.map(city => (
+                                    <option value={city.id} key={city.id}>
+                                        {city.name}
+                                    </option>
+                                ))}
+                            </>
+                            :
+                            <></>
+                            }
                         </select>
                     </div>
                 </div>
                 <div className="search-two">
                     <div class="style">
-                        <select>
+                        <select onChange={handleRegionId}>
                             <option>전체</option>
-                            <option> 선택 2 </option>
-                            <option> 선택 3 </option>
-                            <option> 선택 4 </option>
-                            <option> 선택 5 </option>
+                            {regionList != null && !regionListLoading ?
+                            <>
+                                {regionList.data.items.map(region => (
+                                    <option value={region.id} key={region.id}>
+                                        {region.name}
+                                    </option>
+                                ))}
+                            </>
+                            :
+                            <>
+                            </>
+                            }
                         </select>
                     </div>
                 </div>
@@ -59,17 +145,25 @@ const FestivalCategoryList = () => {
 
             {/* Temp */}
             <div className="festival-area-list">
-                <div className="festival-area">
-                    <img src="https://minio.nculture.org/amsweb-opt/multimedia_assets/134/30188/17055/c/080_-2019-%ED%95%A8%ED%8F%89%EB%82%98%EB%B9%84%EC%B6%95%EC%A0%9C-full-size.jpg" alt="test" />
+                {landmarkList != null ?
+                <>
+                    {landmarkList.map(landmark => (
+                        <div className="festival-area" key={landmark.id}>
+                            <img src={landmark.img} alt="test" />
 
-                    <div>
-                        <span className="title">함평 나비 대축제</span>
-                        <div className="hashtag-wrap">
-                            <span className="hashtag">#함평</span>
-                            <span className="hashtag">#나비</span>
+                            <div>
+                                <span className="title">{landmark.name}</span>
+                                <div className="hashtag-wrap">
+                                    {/* <span className="hashtag">#함평</span>
+                                    <span className="hashtag">#나비</span> */}
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
+                    ))}
+                </>
+                :
+                <></>
+                }
 
             </div>
         </div>
