17f3212c9cc57fea9e730983b080b4a7aa1a7bcb
kkj53051000 <tkarnrwl7418@naver.com>
2022-06-04 17:39:36 +0900

    fix Footer : add Footer Logos

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,12 @@ import "../css/Footer.css";
 const Footer = () => {
     return (
         <div className="footer-wrap">
-            <div>
+            <div className="footer-content">
+                <div className="footer-logo-wrap">
+                    <img src="/images/footer_kto.png" alt="" />
+                    <img src="/images/카카.png" alt="" />
+                    <img src="/images/tour.png" alt="" />
+                </div>
 
             </div>
         </div>
